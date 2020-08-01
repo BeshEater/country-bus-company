2f70bb34d3a3c1e8cbda07862ac90d32440c948b
BeshEater <beinzone@gmail.com>
2020-08-02 03:53:56 +0600

    Add sample Maven project stub

---
+++ src/main/java/com/besheater/training/countrybuscompany/App.java
@@ -0,0 +1,7 @@
+package com.besheater.training.countrybuscompany;
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/besheater/training/countrybuscompany/AppTest.java
@@ -0,0 +1,13 @@
+package com.besheater.training.countrybuscompany;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void myVeryThoroughTest() {
+        assertTrue(true);
+    }
+}
