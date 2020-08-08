b9e91afdaf522d0f5817d2151a3121cf95dc66b9
Ilgiz Shakirov <beinzone@gmail.com>
2020-08-08 06:51:56 +0600

    Refactor configuration properties usage

--- src/main/java/com/besheater/training/countrybuscompany/RootContextConfig.java
+++ src/main/java/com/besheater/training/countrybuscompany/RootContextConfig.java
@@ -4,20 +4,32 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan("com.besheater.training.countrybuscompany")
 public class RootContextConfig {
     private static final Logger LOG = LogManager.getLogger();
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public DataSource dataSource() {
-        HikariConfig config = new HikariConfig("/application.properties");
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(env.getProperty("datasource.jdbcUrl"));
+        config.setDriverClassName(env.getProperty("datasource.driverClassName"));
+        config.setUsername(env.getProperty("dataSource.user"));
+        config.setPassword(env.getProperty("dataSource.password"));
+
         return new HikariDataSource(config);
     }
 }
